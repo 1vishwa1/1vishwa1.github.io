3a06e9810906bd48861cdd05fc2aa6f4777efb86
Vishwanath Basavaraj Badiger <159651944+1vishwa1@users.noreply.github.com>
2025-07-06 15:19:33 -0400

    1app.py

--- 1app.py
+++ 1app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPwkKOqPZio9e4xk81ntAv2",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
